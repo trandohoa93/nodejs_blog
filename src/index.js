3c85cbc55692efebff4bb6170190dfa3b5f87a65
trandohoa93 <trandohoa93@gmail.com>
2025-12-29 15:43:04 +0700

    routes

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ const app = express();
 const { engine } = require("express-handlebars");
 const PORT = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //template engine
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+route(app);
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
